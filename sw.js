66347935b106c85e8e657bb9b18662b24785c9b9
Karla Turcios <kmturcios@yahoo.com>
2018-08-12 00:34:34 -0400

    a fetch event listener to the service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var staticCacheName = 'my-site-cache-v9';
+var staticCacheName = 'my-site-cache-v14';
 
 var urlsToCache = [
   '/',
@@ -22,7 +22,10 @@ var urlsToCache = [
   'img/8.jpg',
   'img/8.jpg',
   'img/na.png',
-  'img/na.jpg'
+  'img/na.jpg',
+  'favicon.ico',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+  'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png'
 ];
 self.addEventListener('install', function(event) {
   // Perform install steps
@@ -35,6 +38,35 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(staticCacheName)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
+/*
 self.addEventListener('fetch', function(event) {
   console.log('Fetch event for ', event.request.url);
   event.respondWith(
@@ -61,7 +93,7 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
-
+*/
 self.addEventListener('activate', function(event) {
   console.log('Activating new service worker...');
 
