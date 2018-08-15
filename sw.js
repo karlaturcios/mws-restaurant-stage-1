ea63186a465e1758b74bccaeb1341295cb7021d9
Karla Turcios <kmturcios@yahoo.com>
2018-08-15 15:44:41 -0400

    attempting to clean up sw to zero in on bug in sw and map

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var staticCacheName = 'my-site-cache-va';
+var staticCacheName = 'my-site-cache-v24';
 
 var urlsToCache = [
   '/',
@@ -26,13 +26,14 @@ var urlsToCache = [
   'favicon.ico'
 ];
 self.addEventListener('install', function(event) {
-  // Perform install steps
+  // Perform install steps and set up cache for service worker
   console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
     caches.open(staticCacheName)
       .then(function(cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
+        console.log('Added cache urls');
       })
   );
 });
@@ -42,6 +43,8 @@ self.addEventListener('fetch', function(event) {
     caches.match(event.request)
     .then(function(response) {
       return response || fetchAndCache(event.request);
+      /*return response;*/
+      console.log ('Response returned or fecthandcache event');
     })
   );
 });
@@ -52,10 +55,14 @@ function fetchAndCache(url) {
     // Check if we received a valid response
     if (!response.ok) {
       throw Error(response.statusText);
+      console.log('fetch url not ok');
+      console.log('Response' + response);
     }
     return caches.open(staticCacheName)
     .then(function(cache) {
       cache.put(url, response.clone());
+      console.log('fetch url ok');
+      console.log('Response' + response);      
       return response;
     });
   })
@@ -64,35 +71,9 @@ function fetchAndCache(url) {
     // You could return a custom offline 404 page here
   });
 }
-/*
-self.addEventListener('fetch', function(event) {
-  console.log('Fetch event for ', event.request.url);
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        console.log('Found ', event.request.url, ' in cache');
-        return response;
-      }
-      console.log('Network request for ', event.request.url);
-      return fetch(event.request).then(function(response) {
-        if (response.status === 404) {
-          return caches.match('img/na.png');
-        }
-        return caches.open(staticCacheName).then(function(cache) {
-          if (event.request.url.indexOf('test') < 0) {
-            cache.put(event.request.url, response.clone());
-          }
-          return response;
-        });
-      });
-    }).catch(function(error) {
-      console.log('Error, ', error);
-      return caches.match('img/na.jpg');
-    })
-  );
-});
-*/
+
 self.addEventListener('activate', function(event) {
+  //this service takes center stage now
   console.log('Activating new service worker...');
 
   var cacheWhitelist = [staticCacheName];
@@ -109,87 +90,3 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
-
-
-/*
-self.addEventListener('fetch', function(event) {
-  console.log('Fetch event for ', event.request.url);
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          console.log('Found ', event.request.url, ' in cache');
-          caches.open('my-site-cache-v6').then(function (cache) {
-            cache.put(event.request, response.clone());
-          });
-          return response;
-        }
-        return fetch(event.request);
-        //console.log(resourceType);
-      }//.catch(() => offlineResponse(opts))
-    )
-  );
-});
-self.addEventListener('message', function(event) {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
-});
-*/
-
-/*/
-function offlineResponse (resourceType, opts) {
-  if (resourceType === 'image') {
-    // … return an offline image
-  } else if (resourceType === 'content') {
-    return caches.match('/offline/');
-  }
-  return undefined;
-}
-*/
-
-/*
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      return response || fetch(event.request);
-    }).catch(error => {
-      if (event.request.url.indexOf(".jpg") > -1) {
-        return caches.match("/img/na.png");
-      }
-      return new Response("Application is not connected to the internet", {
-        status: 404,
-        statusText: "Application is not connected to the internet"
-      });
-    });  
-  );
-});
-*/
-/*
-const handleNonAJAXEvent = (event, cacheRequest) => {
-  // Check if the HTML request has previously been cached. If so, return the
-  // response from the cache. If not, fetch the request, cache it, and then return
-  // it.
-  event.respondWith(caches.match(cacheRequest).then(response => {
-    return (response || fetch(event.request).then(fetchResponse => {
-      return caches
-        .open(cacheID)
-        .then(cache => {
-          if (fetchResponse.url.indexOf("browser-sync") === -1) {
-            cache.put(event.request, fetchResponse.clone());
-          }
-          return fetchResponse;
-        });
-    }).catch(error => {
-      if (event.request.url.indexOf(".jpg") > -1) {
-        return caches.match("/img/na.png");
-      }
-      return new Response("Application is not connected to the internet", {
-        status: 404,
-        statusText: "Application is not connected to the internet"
-      });
-    }));
-  }));
-};*/
